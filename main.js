1fda003ce692e286337a5240a93151667cb8265a
LisandroCausa <lisandro.anexa@gmail.com>
2022-03-30 20:50:15 -0300

    Add input and output system type select

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
 const userInput = document.querySelector('#user-input');
 const resultText = document.querySelector('#result');
+const inputSelect = document.querySelector('#input-select');
+const outputSelect = document.querySelector('#output-select');
 
 function convertBinaryToDecimal(binary = '0') {
     binary = String(binary);
     let decimal = 0;
     for(let i = 0; i < binary.length; i++)
     {
-        if(binary[i] === '0')
-            continue;
-        
-        decimal += 2**(binary.length - i - 1);
+        decimal += Number(binary[i]) * 2**(binary.length - i - 1);
     }
     return decimal;
+}
+
+function convertToBinary(number = 0) {
+    number = Math.abs(number);
+    let maxPower2Subtractable = 0;
+    for(let i = 0; i < 1024; i++)
+    {
+        if(number < 2**i)
+        {
+            maxPower2Subtractable = i-1;
+            break;
+        }
+    }
+
+    let result = '';
+    for(let i = maxPower2Subtractable; i >= 0; i--)
+    {
+        const power2 = 2**i;
+        if(number >= power2)
+        {
+            number -= power2;
+            result += '1';
+        }
+        else
+        {
+            result += '0';
+        }
+    }
+    return result;
 }
 
 function isBinary(binary) {
@@ -25,9 +53,32 @@ function isBinary(binary) {
 
 function updateResult() {
     const inputValue = userInput.value;
-    if(!isBinary(inputValue))
-        return;
-    
-    const result = convertBinaryToDecimal(inputValue);
-    resultText.textContent = result;
+    const inputSystem = inputSelect.value;
+    const outputSystem = outputSelect.value;
+
+    let result;
+    switch(inputSystem)
+    {
+    case 'binary':
+        if(!isBinary(inputValue))
+            return;
+        result = convertBinaryToDecimal(inputValue);
+        break;
+    case 'decimal':
+        if(Number.isNaN(Number(inputValue))) 
+            return;
+        result = inputValue;
+        break;
+    }
+
+    let convertedResult = result;
+    switch(outputSystem)
+    {
+    case 'binary':
+        convertedResult = convertToBinary(result);
+        break;
+    case 'decimal':
+        break;
+    }
+    resultText.textContent = convertedResult;
 }
